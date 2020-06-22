299f154d3213436f1e2eae1d7bd13d2bc31f7ff1
shishir75 <shishir.srdr16@gmail.com>
2020-06-23 01:25:21 +0600

    Installing Laravel Passport for API Auth

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,7 @@ import { Form, HasError, AlertError } from 'vform';
 import moment from "moment";
 import VueProgressBar from 'vue-progressbar';
 import Swal from 'sweetalert2';
+import Developer from "./components/Developer";
 
 require('./bootstrap');
 
@@ -43,7 +44,8 @@ Vue.use(VueProgressBar, {
 let routes = [
     { path: '/dashboard', component: Dashboard },
     { path: '/profile', component: Profile },
-    { path: '/users', component: Users }
+    { path: '/users', component: Users },
+    { path: '/developer', component: Developer },
 ];
 
 
@@ -64,6 +66,24 @@ Vue.filter('myDate', (created) => {
 
 // new vue instance
 window.Fire = new Vue();
+
+// passport vue component start
+Vue.component(
+    'passport-clients',
+    require('./components/passport/Clients.vue').default
+);
+
+Vue.component(
+    'passport-authorized-clients',
+    require('./components/passport/AuthorizedClients.vue').default
+);
+
+Vue.component(
+    'passport-personal-access-tokens',
+    require('./components/passport/PersonalAccessTokens.vue').default
+);
+
+// passport vue component end
 
 const app = new Vue({
     el: '#app',
