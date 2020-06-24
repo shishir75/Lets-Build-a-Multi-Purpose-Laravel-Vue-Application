5f39d236b323096e5286f916e24bccec243e9c7c
shishir75 <shishir.srdr16@gmail.com>
2020-06-25 00:34:59 +0600

    Limit Access to Controllers using ACL in Laravel

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Hash;
 use Intervention\Image\Facades\Image;
 
@@ -147,6 +148,8 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
+        Gate::authorize('isAdmin');
+
         $user = User::findOrFail($id)->delete();
 
 
