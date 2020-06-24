2d7aeebe36e9892bfa5cebf5a56ab4417bc9ad8e
shishir75 <shishir.srdr16@gmail.com>
2020-06-25 00:21:01 +0600

    Add Gate ACL in Laravel

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -92,22 +92,18 @@ scratch. This page gets rid of all links and provides the needed markup only.
                                     <p>Users</p>
                                 </router-link>
                             </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link">
-                                    <i class="fas fa-circle nav-icon"></i>
-                                    <p>Inactive Page</p>
-                                </a>
-                            </li>
                         </ul>
                     </li>
-                    <li class="nav-item">
-                        <router-link to="/developer" class="nav-link">
-                            <i class="nav-icon fas fa-cogs"></i>
-                            <p>
-                                Developer
-                            </p>
-                        </router-link>
-                    </li>
+                    @can('isAdmin')
+                        <li class="nav-item">
+                            <router-link to="/developer" class="nav-link">
+                                <i class="nav-icon fas fa-cogs"></i>
+                                <p>
+                                    Developer
+                                </p>
+                            </router-link>
+                        </li>
+                    @endcan
                     <li class="nav-item">
                         <router-link to="/profile" class="nav-link">
                             <i class="nav-icon fas fa-user orange"></i>
